6bdb154c6b2d6586fc317a83b981ee58371bbfef
Chengle <songchengle105019@gmail.com>
2026-08-07 22:39:01 +0800

    完成新车头四轮PI落地直行验证

---
+++ STM32/Keil_Project/NewFront_FourMotor_GroundStraight_PI_Test/MDK-ARM/RTE/_Four_Motor_Chassis_Test/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Four_Motor_Chassis_Test' 
+ * Target:  'Four_Motor_Chassis_Test' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
